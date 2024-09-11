eff544cff049d60d50c20fabaab63c1a79dd3867
UABGabri <101285426+UABGabri@users.noreply.github.com>
2024-09-11 19:13:11 +0200

    Register actualizado

--- FrontEnd/frontend/src/App.jsx
+++ FrontEnd/frontend/src/App.jsx
@@ -1,24 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import './App.css'
-import homepage from './homepage'
-import register from './register'
+
+import Homepage from './Homepage'
+import Register from './Register'
 
 function App() {
-
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element = {<homepage />}></Route>
-        <Route path='/' element = {<register />}></Route>
-
+        <Route path='/' element = {< Homepage />} > </Route>
+        <Route path='/' element = {< Register />}></Route>
       </Routes>
     </BrowserRouter>
   )
-      
 }
 
 export default App
