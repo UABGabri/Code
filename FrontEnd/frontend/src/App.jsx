77a7da29e5f5cf9427ffa53cca3616bca60b55d1
UABGabri <101285426+UABGabri@users.noreply.github.com>
2024-09-11 19:08:20 +0200

    Register home page

--- FrontEnd/frontend/src/App.jsx
+++ FrontEnd/frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import './App.css'
+import homepage from './homepage'
+import register from './register'
 
 function App() {
 
 
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element = {<homepage />}></Route>
+        <Route path='/' element = {<register />}></Route>
+
+      </Routes>
+    </BrowserRouter>
   )
       
 }
