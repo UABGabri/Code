09b5eaa0ca3692f2ace27d62f15816a4598e06cd
UABGabri <101285426+UABGabri@users.noreply.github.com>
2024-09-12 19:07:56 +0200

    Change Names

--- FrontEnd/frontend/src/Login.jsx
+++ FrontEnd/frontend/src/Login.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
 
 function Login() {
+  
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
