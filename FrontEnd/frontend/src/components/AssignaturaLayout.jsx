a2a5c6ff108909d6bf9225f3bc41cbe5ce816601
UABGabri <101285426+UABGabri@users.noreply.github.com>
2024-11-01 00:17:57 +0100

    LAYOUT BÁSIC ASSIGNATURES

--- FrontEnd/frontend/src/components/AssignaturaLayout.jsx
+++ FrontEnd/frontend/src/components/AssignaturaLayout.jsx
@@ -1,15 +1,29 @@
-import { useParams, useLocation } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import Headercap from "./Headercap";
+import styles from "./AssignaturaLayout.module.css";
+import { BiArrowBack } from "react-icons/bi";
 
 function AssignaturaLayout() {
   const { id } = useParams();
   const location = useLocation();
   const { name } = location.state;
+  const history = useNavigate();
 
   return (
     <div>
-      <h1>Detalls de la Asignatura</h1>
-      <h2>ID: {id}</h2>
-      <h2>Nom: {name}</h2>
+      <Headercap />
+      <header className={styles.headerSubject}>
+        <BiArrowBack onClick={() => history(-1)} className={styles.backArrow} />
+        <span>{name}</span>
+        <span>{id}</span>
+      </header>
+
+      <div className={styles.mainMenu}>
+        <span>CURS</span>
+        <span>PARTICIPANTS</span>
+        <span>PREGUNTES</span>
+        <span>TESTS</span>
+      </div>
     </div>
   );
 }
