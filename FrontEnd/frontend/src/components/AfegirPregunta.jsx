432679c6b1f40d2c17e40fc25987010ce9ef7dfd
UABGabri <101285426+UABGabri@users.noreply.github.com>
2024-11-07 18:54:24 +0100

    CSS FINAL PREGUNTA

--- FrontEnd/frontend/src/components/AfegirPregunta.jsx
+++ FrontEnd/frontend/src/components/AfegirPregunta.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styles from "./AfegirPregunta.module.css";
 import Headercap from "./Headercap";
 import { BiArrowBack } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
 function AfegirPregunta() {
   const [values, setValues] = useState({
@@ -15,6 +16,8 @@ function AfegirPregunta() {
     erronea_3: "",
     erronea_4: "",
   });
+
+  const history = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -30,119 +33,128 @@ function AfegirPregunta() {
   };
 
   return (
-    <div className={styles.content}>
+    <div>
       <Headercap />
-      <BiArrowBack onClick={() => history(-1)} className={styles.backArrow} />
-      <h1>AFEGIR PREGUNTA</h1>
-      <form onSubmit={handleSubmit} className={styles.addQuestionForm}>
-        <div className={styles.formGroup}>
-          <label htmlFor="tema">Tema:</label>
-          <input
-            type="text"
-            id="tema"
-            name="tema"
-            value={values.tema}
-            onChange={handleChange}
-          />
-        </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="conceptes">Conceptes:</label>
-          <input
-            type="text"
-            id="conceptes"
-            name="conceptes"
-            value={values.conceptes}
-            onChange={handleChange}
-            placeholder="Conceptes separats per comes"
-          />
-        </div>
+      <div className={styles.content}>
+        <BiArrowBack onClick={() => history(-1)} className={styles.arrowBack} />
 
-        <div className={styles.formGroup}>
-          <label htmlFor="dificultat">Dificultat:</label>
-          <select
-            id="dificultat"
-            name="dificultat"
-            value={values.dificultat}
-            onChange={handleChange}
-          >
-            <option value="">Selecciona</option>
-            <option value="Fàcil">Fàcil</option>
-            <option value="Mitjà">Mitjà</option>
-            <option value="Difícil">Difícil</option>
-          </select>
-        </div>
+        <h1>AFEGIR PREGUNTA</h1>
+        <form onSubmit={handleSubmit} className={styles.addQuestionForm}>
+          <div className={styles.formGroup}>
+            <label htmlFor="tema">Tema:</label>
+            <input
+              type="text"
+              id="tema"
+              name="tema"
+              value={values.tema}
+              onChange={handleChange}
+              placeholder="Introdueix un tema vàlid"
+            />
+          </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="pregunta">Pregunta:</label>
-          <textarea
-            id="pregunta"
-            name="pregunta"
-            value={values.pregunta}
-            onChange={handleChange}
-            className={styles.textarea}
-          />
-        </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="conceptes">Conceptes:</label>
+            <input
+              type="text"
+              id="conceptes"
+              name="conceptes"
+              value={values.conceptes}
+              onChange={handleChange}
+              placeholder="Conceptes separats per comes"
+            />
+          </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="solucio">Solució Correcta:</label>
-          <input
-            type="text"
-            id="solucio"
-            name="solucio"
-            value={values.solucio}
-            onChange={handleChange}
-          />
-        </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="dificultat">Dificultat:</label>
+            <select
+              id="dificultat"
+              name="dificultat"
+              value={values.dificultat}
+              onChange={handleChange}
+            >
+              <option value="">Selecciona</option>
+              <option value="Fàcil">Fàcil</option>
+              <option value="Mitjà">Mitjà</option>
+              <option value="Difícil">Difícil</option>
+            </select>
+          </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="erronea_1">Solució Incorrecta 1:</label>
-          <input
-            type="text"
-            id="erronea_1"
-            name="erronea_1"
-            value={values.erronea_1}
-            onChange={handleChange}
-          />
-        </div>
+          <div className={styles.questionArea}>
+            <div className={styles.formGroup}>
+              <label htmlFor="pregunta">Pregunta:</label>
+              <textarea
+                id="pregunta"
+                name="pregunta"
+                value={values.pregunta}
+                onChange={handleChange}
+                className={styles.textarea}
+              />
+            </div>
+          </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="erronea_2">Solució Incorrecta 2:</label>
-          <input
-            type="text"
-            id="erronea_2"
-            name="erronea_2"
-            value={values.erronea_2}
-            onChange={handleChange}
-          />
-        </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="solucio">Solució Correcta:</label>
+            <input
+              type="text"
+              id="solucio"
+              name="solucio"
+              value={values.solucio}
+              onChange={handleChange}
+            />
+          </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="erronea_3">Solució Incorrecta 3:</label>
-          <input
-            type="text"
-            id="erronea_3"
-            name="erronea_3"
-            value={values.erronea_3}
-            onChange={handleChange}
-          />
-        </div>
+          <div className={styles.wrongArea}>
+            <div className={styles.formGroup}>
+              <label htmlFor="erronea_1">Solució Incorrecta 1:</label>
+              <input
+                type="text"
+                id="erronea_1"
+                name="erronea_1"
+                value={values.erronea_1}
+                onChange={handleChange}
+              />
+            </div>
 
-        <div className={styles.formGroup}>
-          <label htmlFor="erronea_4">Solució Incorrecta 4:</label>
-          <input
-            type="text"
-            id="erronea_4"
-            name="erronea_4"
-            value={values.erronea_4}
-            onChange={handleChange}
-          />
-        </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="erronea_2">Solució Incorrecta 2:</label>
+              <input
+                type="text"
+                id="erronea_2"
+                name="erronea_2"
+                value={values.erronea_2}
+                onChange={handleChange}
+              />
+            </div>
 
-        <button type="submit" className={styles.sendButton}>
-          AFEGIR
-        </button>
-      </form>
+            <div className={styles.formGroup}>
+              <label htmlFor="erronea_3">Solució Incorrecta 3:</label>
+              <input
+                type="text"
+                id="erronea_3"
+                name="erronea_3"
+                value={values.erronea_3}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="erronea_4">Solució Incorrecta 4:</label>
+              <input
+                type="text"
+                id="erronea_4"
+                name="erronea_4"
+                value={values.erronea_4}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          <button type="submit" className={styles.sendButton}>
+            AFEGIR
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
