101100a78d2fa8142285d232887f3a3fd580c54c
UABGabri <101285426+UABGabri@users.noreply.github.com>
2025-01-20 13:25:56 +0100

    CANVIS

--- FrontEnd/frontend/src/components/Register.jsx
+++ FrontEnd/frontend/src/components/Register.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import styles from "./StyleComponents/Homepage.module.css";
+import styles from "./Estils.module.css";
 
 function Register() {
   // Emmagatzemem les dades del formulari
